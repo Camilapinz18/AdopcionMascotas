6ee238fc0b8291c811b1d7bb9cba7b330591a3c8
Camila Pinzon <camilapinz18@gmail.com>
2023-02-21 10:18:29 -0500

    funcional

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 const app = Vue.createApp({
   data () {
     return {
+      imagesToUploadDogs: [
+        {
+          url: './images/perro1.jpg',
+          id: 1
+        },
+        {
+          url: './images/perro2.jpg',
+          id: 2
+        },
+        {
+          url: './images/perro3.jpg',
+          id: 3
+        },
+        {
+          url: './images/perro4.png',
+          id: 4
+        }
+      ],
+      imagesToUploadCats: [
+        {
+          url: './images/gato1.jpg',
+          id: 1
+        },
+        {
+          url: './images/gato2.png',
+          id: 2
+        },
+        {
+          url: './images/gato3.jpg',
+          id: 3
+        },
+        {
+          url: './images/gato4.jpg',
+          id: 4
+        }
+      ],
       users: [],
       availablePets: [
         {
@@ -35,6 +71,39 @@ const app = Vue.createApp({
           description: 'Funny',
           image: './images/perro3.jpg',
           status: 'available'
+        },
+        {
+          name: 'Pepe',
+          specie: 'dog',
+          breed: 'creole',
+          gender: 'male',
+          color: 'black',
+          age: '6',
+          description: 'Funny',
+          image: './images/perro1.jpg',
+          status: 'available'
+        },
+        {
+          name: 'Luna',
+          specie: 'dog',
+          breed: 'creole',
+          gender: 'female',
+          color: 'Brown',
+          age: '6',
+          description: 'Funny',
+          image: './images/perro2.jpg',
+          status: 'available'
+        },
+        {
+          name: 'Shaggy',
+          specie: 'dog',
+          breed: 'creole',
+          gender: 'female',
+          color: 'white',
+          age: '6',
+          description: 'Funny',
+          image: './images/perro3.jpg',
+          status: 'available'
         }
       ],
       adoptedPets: [],
@@ -46,13 +115,15 @@ const app = Vue.createApp({
       isAdminList: false,
       isPetList: false,
       currentUser: null,
+      isAdmin: false,
       name: null,
       breed: null,
       color: null,
       age: null,
       specie: null,
       gender: null,
-      description: null
+      description: null,
+      image: null
     }
   },
   methods: {
@@ -117,6 +188,21 @@ const app = Vue.createApp({
         return 'client'
       }
     },
+    uploadImage () {
+      console.log('Specie', this.specie)
+      if (
+        this.specie === '' ||
+        this.specie === undefined ||
+        this.specie === null
+      ) {
+        alert('please select a specie')
+      } else {
+      }
+    },
+    assignImage (image) {
+      this.image = image
+      console.log('IMAGE', this.image)
+    },
     login () {
       console.log('LOGINM', this.username, this.password)
       if (
@@ -145,6 +231,7 @@ const app = Vue.createApp({
               this.currentUser = userExists
               this.isLogin = false
               this.isLandPage = true
+              this.isPetList = true
             }
           } else {
             alert('wrong password')
@@ -162,6 +249,13 @@ const app = Vue.createApp({
         this.currentUser = isUserLogged
         this.isLogin = false
         this.isLandPage = true
+        if (isUserLogged.role === 'admin') {
+          this.isAdmin = true
+          console.log(this.isAdmin, 'admin')
+        } else if (isUserLogged.role === 'client') {
+          this.isAdmin = false
+          console.log(this.isAdmin, 'admin')
+        }
       } else {
         this.isLogin = true
         this.isLandPage = false
@@ -180,8 +274,8 @@ const app = Vue.createApp({
     },
     giveForAdoption () {
       console.log('give')
-
-      this.isGiveForAdoption = !this.isGiveForAdoption
+      this.isPetList = false
+      this.isGiveForAdoption = true
     },
     addPet () {
       console.log('add')
@@ -218,29 +312,48 @@ const app = Vue.createApp({
           color: this.color,
           age: this.age,
           description: this.description,
-          image: './images/perro2.jpg',
+          image: this.image.url,
           status: 'available'
         })
 
         alert('Pet added correctly')
         console.log('PETS', this.availablePets)
         localStorage.setItem('pets', JSON.stringify(this.availablePets))
+
+        this.specie = null
+        this.name = null
+        this.breed = null
+        this.color = null
+        this.age = null
+        this.gender = null
+        this.description = null
+        this.image = null
       }
     },
     syncLocalStorage () {
       if (
         localStorage.getItem('pets') === null ||
-        localStorage.getItem('pets') === undefined
+        localStorage.getItem('pets') === undefined ||
+        localStorage.getItem('adoptedPets') === null ||
+        localStorage.getItem('adoptedPets') === undefined
       ) {
         localStorage.setItem('pets', JSON.stringify(this.availablePets))
+        localStorage.setItem('adoptedPets', JSON.stringify(this.adoptedPets))
       } else {
         localStorage.setItem('pets', localStorage.getItem('pets'))
         const toUpdatePets = JSON.parse(localStorage.getItem('pets'))
         this.availablePets = toUpdatePets
+
+        localStorage.setItem('adoptedPets', localStorage.getItem('adoptedPets'))
+        const toUpdateAdoptedPets = JSON.parse(
+          localStorage.getItem('adoptedPets')
+        )
+        this.adoptedPets = toUpdateAdoptedPets
       }
     },
     showAdopt () {
-      this.isPetList = !this.isPetList
+      this.isPetList = true
+      this.isGiveForAdoption = false
     },
     adopt (pet) {
       const result = confirm('Are you sure you want to adopt this pet?')
@@ -263,6 +376,9 @@ const app = Vue.createApp({
 
         localStorage.setItem('pets', JSON.stringify(this.availablePets))
       }
+    },
+    showAdminList () {
+      this.isAdminList = true
     }
   },
 
@@ -271,6 +387,8 @@ const app = Vue.createApp({
     this.fetchUsers()
     this.checkIfUserLogged()
     this.syncLocalStorage()
+    console.log('adoptedPetsss', this.adoptedPets)
+    
   }
 })
 
